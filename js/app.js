b66e2e1c704cf453734db7bd6aeed0e2f9608e17
Arek <areksonic@gmail.com>
2024-09-29 23:17:32 +0200

    oh god

--- js/app.js
+++ js/app.js
@@ -1,14 +1,40 @@
 const timetable = [
-    ["Math", "English", "Physics", "History", "Geography"],   // Period 1
-    ["Science", "Math", "PE", "Chemistry", "Music"],          // Period 2
-    ["English", "History", "Biology", "Math", "Computer"],    // Period 3
-    ["Free", "Chemistry", "Free", "PE", "Free"],              // Period 4
-    ["Math", "Physics", "Free", "History", "English"],        // Period 5
-    ["Computer", "Math", "Free", "Science", "Music"],         // Period 6
-    ["Free", "Free", "Geography", "Math", "Free"],            // Period 7
-    ["Math", "Free", "Computer", "Free", "Physics"],          // Period 8
-    ["Free", "PE", "Free", "History", "Math"],                // Period 9
-    ["Biology", "Math", "Free", "Chemistry", "Free"],         // Period 10
-    ["Free", "Free", "Free", "Free", "Free"],                 // Period 11
+    ["Free", "Free", "Free", "Free", "Free"],   // Period 1
+    ["Free", "Free", "Free", "Free", "Free"],   // Period 2
+    ["Free", "Free", "Free", "Free", "Free"],   // Period 3
+    ["Free", "Free", "Free", "Free", "Free"],   // Period 4
+    ["Free", "Free", "Free", "Free", "Free"],   // Period 5
+    ["Free", "Free", "Free", "Free", "Free"],   // Period 6
+    ["Free", "Free", "Free", "Free", "Free"],   // Period 7
+    ["Free", "Free", "Free", "Free", "Free"],   // Period 8
+    ["Free", "Free", "Free", "Free", "Free"],   // Period 9
+    ["Free", "Free", "Free", "Free", "Free"],   // Period 10
+    ["Free", "Free", "Free", "Free", "Free"]    // Period 11
 ];
 
+let weekdays = ["mo", "tu", "we", "th", "fr"]
+
+let subjects = [];
+let fillCount = 0;
+
+
+function addSubject() {
+    const name = prompt("Insert subject name:");
+    const repetition = parseInt(prompt("Insert repetition"))
+
+    const newSubject = {
+        name: name,
+        repetition: repetition
+    }
+    subjects.push(newSubject)
+    console.log(subjects)
+}
+
+function arrange() {
+    subjects.forEach(subject => {
+        for (let temp = 0; temp < subject.repetition; temp++) {
+            document.getElementById(fillCount).innerHTML = subject.name
+            fillCount++
+        }
+    });
+}
