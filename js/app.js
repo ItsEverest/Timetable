b9662886aa896b394c3329f0ad295a3cc915f647
Arek <areksonic@gmail.com>
2024-09-29 21:57:43 +0200

    e

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const timetable = [
+    ["Math", "English", "Physics", "History", "Geography"],   // Period 1
+    ["Science", "Math", "PE", "Chemistry", "Music"],          // Period 2
+    ["English", "History", "Biology", "Math", "Computer"],    // Period 3
+    ["Free", "Chemistry", "Free", "PE", "Free"],              // Period 4
+    ["Math", "Physics", "Free", "History", "English"],        // Period 5
+    ["Computer", "Math", "Free", "Science", "Music"],         // Period 6
+    ["Free", "Free", "Geography", "Math", "Free"],            // Period 7
+    ["Math", "Free", "Computer", "Free", "Physics"],          // Period 8
+    ["Free", "PE", "Free", "History", "Math"],                // Period 9
+    ["Biology", "Math", "Free", "Chemistry", "Free"],         // Period 10
+    ["Free", "Free", "Free", "Free", "Free"],                 // Period 11
+];
+
