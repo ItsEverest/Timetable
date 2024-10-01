91a97185b77de3ecc9cad97cfd39efae47fda741
Arek <areksonic@gmail.com>
2024-10-01 21:35:50 +0200

    LES GOOO

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const timetable = [
+const dummyTimetable = [
     ["Free", "Free", "Free", "Free", "Free"],   // Period 1
     ["Free", "Free", "Free", "Free", "Free"],   // Period 2
     ["Free", "Free", "Free", "Free", "Free"],   // Period 3
@@ -12,7 +12,57 @@ const timetable = [
     ["Free", "Free", "Free", "Free", "Free"]    // Period 11
 ];
 
-let subjects = []
+const timetable = [
+    [
+    ["Free", "Free", "Free", "Free", "Free"],   // Period 1
+    ["Free", "Free", "Free", "Free", "Free"],   // Period 2
+    ["Free", "Free", "Free", "Free", "Free"],   // Period 3
+    ["Free", "Free", "Free", "Free", "Free"],   // Period 4
+    ["Free", "Free", "Free", "Free", "Free"],   // Period 5
+    ["Free", "Free", "Free", "Free", "Free"],   // Period 6
+    ["Free", "Free", "Free", "Free", "Free"],   // Period 7
+    ["Free", "Free", "Free", "Free", "Free"],   // Period 8
+    ["Free", "Free", "Free", "Free", "Free"],   // Period 9
+    ["Free", "Free", "Free", "Free", "Free"],   // Period 10
+    ["Free", "Free", "Free", "Free", "Free"]    // Period 11
+    ]
+];
+
+let subjects = [[]]
+let classesNames = ["default"]
+
+let currentClassId = 0
+let classIdCounter = 0
+
+// Adds class to the name arrays and ID arrays and inserts new slots for new timetable in main array
+function addClass() {
+    let name = prompt("Input your classname")
+    if(name === null || name.trim() === "") {
+        alert("Wrong type of value!")
+    } else {
+        classesNames.push(name)
+        subjects.push([])
+        timetable.push(dummyTimetable)
+        classIdCounter += 1
+
+        console.log(classesNames)
+        console.log("classIdCounter: " + classIdCounter)
+        console.log(timetable)
+    }
+}
+
+// Selects class
+function selectClass() {
+    let temp = prompt("Input your class")
+    if(temp === null || temp.trim() === "" || classesNames.includes(temp) == false) {
+        alert("Wrong type of value!")
+    } else {
+        currentClassId = classesNames.indexOf(temp)
+
+        console.log("currentClassId: " + currentClassId)
+    }
+
+}
 
 
 // Adding subjects to the array, checking validity of name and repetition throughout the week
@@ -27,7 +77,9 @@ function addSubject() {
                 name: name,
                 repetition: repetition
             }
-            subjects.push(newSubject)
+            subjects[currentClassId].push(newSubject)
+
+            console.log(subjects)
         } else {
             alert("Wrong type of value!")
         }
@@ -38,7 +90,7 @@ function addSubject() {
 function arrange() {
     for (let i = 0; i < 11; i++) {
         for (let j = 0; j < 5; j++) {
-            timetable[i][j] = "Empty"
+            timetable[currentClassId][i][j] = "Empty"
         }
     }
 
@@ -47,13 +99,13 @@ function arrange() {
     // Filling out the timetable array an keeping it going out of bounds
     let dummyCounterWidth = 0
     let dummyCounterHeight = 0
-    for (let i = 0; i < subjects.length; i++) {
-        for (let j = 0; j < subjects[i].repetition; j++) {
+    for (let i = 0; i < subjects[currentClassId].length; i++) {
+        for (let j = 0; j < subjects[currentClassId][i].repetition; j++) {
             if (dummyCounterHeight>=11) {
                 alert("Timetable out of range!")
                 return
             }
-            timetable[dummyCounterHeight][dummyCounterWidth] = subjects[i].name
+            timetable[currentClassId][dummyCounterHeight][dummyCounterWidth] = subjects[currentClassId][i].name
             dummyCounterWidth += 1
             if (dummyCounterWidth>=5) {
                 dummyCounterWidth = 0
@@ -70,7 +122,7 @@ function display() {
 
     for (let i = 0; i < 11; i++) {
         for (let j = 0; j < 5; j++) {
-            document.getElementById(dummyCounter).innerHTML = timetable[i][j]
+            document.getElementById(dummyCounter).innerHTML = timetable[currentClassId][i][j]
             dummyCounter+=1
         }
     }
